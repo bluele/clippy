307ef1d212ca2f35517a452b4eaf6736ff2b0c77
junkimura <junkxdev@gmail.com>
2013-08-25 11:56:51 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from os.path import dirname, abspath, join, isfile
+
+cwd = abspath(dirname(__file__))
+
+version = '1.0'
+
+setup(name='clippy',
+      version=version,
+      description="",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='clipboard command',
+      author='bluele',
+      author_email='junkxdev@gmail.com',
+      url='',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      scripts=[
+          join(cwd, 'scripts', 'clippy'),
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
